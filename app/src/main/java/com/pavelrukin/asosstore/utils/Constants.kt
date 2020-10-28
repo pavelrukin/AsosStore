e03791bb11cf2665664a77c09e39c44ccc601517
Pavel Rukin <rivelr@gmail.com>
2020-10-28 12:19:33 +0200

    add new API key

--- app/src/main/java/com/pavelrukin/asosstore/utils/Constants.kt
+++ app/src/main/java/com/pavelrukin/asosstore/utils/Constants.kt
@@ -6,6 +6,7 @@ class Constants {
 
         const val API_KEY ="d2c3171046msh39cbcaa34d11378p163e14jsn3a89b6c607a4"
         //"bc47ba0c16msh6584eff46ff8755p1a9edbjsnf8ea806caad9"
+        //"efc291ba21msh61fbf33ebbee103p1093f2jsndeeb64719af2" // new
         const val BASE_URL ="https://asos2.p.rapidapi.com/"
         const val IMG_URL  ="https://"
         const val DATABSE_NAME = "product_db.db"
